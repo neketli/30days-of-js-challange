3bc3ef21a4ec6237884dd492c208b9aa07ef39fb
neketli <neketli.cspl@gmail.com>
2022-04-18 16:47:22 +0300

    Completed day 2

--- day1-piano/app.js
+++ day1-piano/app.js
@@ -6,9 +6,16 @@ window.addEventListener('keydown', (e) => {
 	key.classList.add('active-white');
 })
 
+
 const keys = document.querySelectorAll('.piano__note-white');
-keys.forEach(k => k.addEventListener('transitioned', function(e) {
-		console.log(e);
-		if (e.propertyName !== 'background-color') return;
-		this.classList.remove('active-white');
-}))
+keys.forEach(k => k.addEventListener('transitionend', function (e) {
+	if (e.propertyName !== 'background-color') return;
+	this.classList.remove('active-white');
+}));
+keys.forEach(k => k.addEventListener('click', function (e) {
+	const audio = document.querySelector(`.${e.target.id}`);
+	const key = document.querySelector(`#${e.target.id}`);
+	if (!audio) return;
+	audio.play();
+	key.classList.add('active-white');
+}));
---
+++ day2-clock/app.js
@@ -0,0 +1,16 @@
+const secondsArrow = document.querySelector('.s-arrow');
+const minutesArrow = document.querySelector('.m-arrow');
+const hoursArrow = document.querySelector('.h-arrow');
+
+const setDate = () => {
+	const now = new Date();
+	const s = (now.getSeconds() / 60) * 360 + 90;
+	const m = (now.getMinutes() / 60) * 360 + 90;
+	const h = (now.getHours() / 60) * 360 + 90;
+	
+	secondsArrow.style.transform = `rotate(${s}deg)`;
+	minutesArrow.style.transform = `rotate(${m}deg)`;
+	hoursArrow.style.transform = `rotate(${h}deg)`;
+}
+
+setInterval(setDate, 1000);
